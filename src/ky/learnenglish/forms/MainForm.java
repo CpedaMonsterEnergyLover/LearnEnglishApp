04e5ff93b0edef9e89187755a42f7c3918cf8d97
CpedaMonsterEnergyLover <vova.cpeda@yandex.ru>
2022-11-15 19:41:57 +0600

    dasdsadas

--- src/ky/learnenglish/forms/MainForm.java
+++ src/ky/learnenglish/forms/MainForm.java
@@ -1,6 +1,5 @@
 package ky.learnenglish.forms;
 
-import javafx.scene.paint.Stop;
 import ky.learnenglish.util.ContentLoader;
 import ky.learnenglish.util.Mathf;
 
@@ -23,10 +22,14 @@ public class MainForm extends BaseForm {
     private JLabel wordNumberLabel;
     private JLabel leftPromoLabel;
     private JLabel rightPromoLabel;
+    private JLabel creditsLine1;
+    private JLabel creditsLine2;
+    private JLabel creditsLine3;
+    private JLabel creditsLine4;
     private JPanel labelPanel;
     private JPanel promoPanel;
     private JSlider progressSlider;
-    private JTextArea credits;
+    private JPanel creditsPanel;
     private final List<String> lines;
 
     private final int start;
@@ -34,16 +37,14 @@ public class MainForm extends BaseForm {
     private volatile boolean paused = false;
     private volatile Thread currentThread = null;
 
-
     private static final Color darkTextColor = new Color(60, 63, 65);
-    private static final Color lightTextColor = new Color(187, 187, 187);
 
-//    private Clip clip;
     private final ClassLoader classLoader;
-//    private AudioInputStream ais;
 
     private boolean lessonStarted;
     private final boolean finalWeek;
+
+
     private void removeBackgrounds(){
         mainPanel.setBackground(new Color(Color.TRANSLUCENT));
         mainPanel.setOpaque(false);
@@ -57,15 +58,27 @@ public class MainForm extends BaseForm {
         ruLabel.setBackground(new Color(Color.TRANSLUCENT));
         ruLabel.setOpaque(false);
         ruLabel.repaint();
+        creditsPanel.setBackground(new Color(Color.TRANSLUCENT));
+        creditsPanel.setOpaque(false);
+        creditsPanel.repaint();
         wordNumberLabel.setBackground(new Color(Color.TRANSLUCENT));
         wordNumberLabel.setOpaque(false);
         wordNumberLabel.repaint();
         labelPanel.setBackground(new Color(Color.TRANSLUCENT));
         labelPanel.setOpaque(false);
         labelPanel.repaint();
-        //promoPanel.setBackground(new Color(Color.TRANSLUCENT));
-        //promoPanel.setOpaque(false);
-        //promoPanel.repaint();
+        promoPanel.setBackground(new Color(Color.TRANSLUCENT));
+        promoPanel.setOpaque(false);
+        promoPanel.repaint();
+        leftPromoLabel.setBackground(new Color(Color.TRANSLUCENT));
+        leftPromoLabel.setOpaque(false);
+        leftPromoLabel.repaint();
+        rightPromoLabel.setBackground(new Color(Color.TRANSLUCENT));
+        rightPromoLabel.setOpaque(false);
+        rightPromoLabel.repaint();
+        progressSlider.setBackground(new Color(Color.TRANSLUCENT));
+        progressSlider.setOpaque(false);
+        progressSlider.repaint();
     }
 
     public MainForm(int start, int amount, boolean finalWeek){
@@ -79,17 +92,18 @@ public class MainForm extends BaseForm {
         setSize(new Dimension(screenSize.width, screenSize.height));
         setLocation(0, 0);
         classLoader = getClass().getClassLoader();
-        promoPanel.setVisible(false);
         setContentPane(newPanel);
         PrepareKeyListeners();
+        creditsPanel.setVisible(false);
         setVisible(true);
-
         PrepareSlider();
         StartIntroThread();
     }
 
 
     private void PrepareSlider(){
+        progressSlider.setMinimum(0);
+        progressSlider.setMaximum(1000);
         progressSlider.setVisible(false);
         progressSlider.setValue(0);
         Hashtable<Integer, JLabel> lableTable = new Hashtable<>();
@@ -113,6 +127,7 @@ public class MainForm extends BaseForm {
             public void mouseReleased(MouseEvent e) {
                 SetPause(false);
                 int newValue = progressSlider.getValue();
+                System.out.println("MOUSE RELEASE " + newValue);
                 if (newValue < 100) {
                     int calc = Mathf.Lerp(newValue / 100f, 0, 120);
                     StartMotivatorsThread(calc);
@@ -130,6 +145,11 @@ public class MainForm extends BaseForm {
                     StartLessonTread(calc, 3);
                 }
             }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                System.out.println(progressSlider.getValue());
+            }
         });
 
     }
@@ -139,12 +159,13 @@ public class MainForm extends BaseForm {
         newPanel.SetImage("spiral_2.gif");
         engLabel.setVisible(false);
         ruLabel.setVisible(false);
+        kyrLabel.setVisible(true);
         wordNumberLabel.setVisible(false);
         ruLabel.setForeground(darkTextColor);
         kyrLabel.setForeground(darkTextColor);
         engLabel.setForeground(darkTextColor);
         kyrLabel.setIcon(null);
-        credits.setVisible(false);
+        creditsPanel.setVisible(false);
     }
 
     private void EndLesson(){
@@ -194,44 +215,40 @@ public class MainForm extends BaseForm {
 
     private void SetPause(boolean isPaused){
         if(!lessonStarted) return;
-        System.out.println("Pause");
         paused = isPaused;
         progressSlider.setVisible(isPaused);
-        if(isPaused) PauseMusic();
+        if(isPaused) {
+            PauseMusic();
+            progressSlider.requestFocus();
+        }
         else StartMusic();
     }
 
     private void PrepareKeyListeners(){
         JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(newPanel);
-        System.out.println("Prepare listeners");
         topFrame.setFocusable(true);
-        topFrame.requestFocus();
+//        topFrame.requestFocus();
         topFrame.addKeyListener(new KeyListener() {
             @Override
-            public void keyTyped(KeyEvent e) {
-
-            }
-
+            public void keyTyped(KeyEvent e) { }
             @Override
             public void keyPressed(KeyEvent e) {
                 int keyCode = e.getKeyCode();
-                System.out.println(keyCode);
                 if (keyCode == KeyEvent.VK_SPACE) {
-                    SetPause(!paused);
+                    if(!lessonStarted) {
+                        if(currentThread != null && currentThread.isAlive()) currentThread.interrupt();
+                    } else SetPause(!paused);
                 } else if (keyCode == KeyEvent.VK_ESCAPE && lessonStarted) {
                     dispose();
                     if(currentThread != null && currentThread.isAlive()) {
-                        currentThread.interrupt();
                         StopMusic();
+                        currentThread.interrupt();
                     }
                     new MenuForm();
                 }
             }
-
             @Override
-            public void keyReleased(KeyEvent e) {
-
-            }
+            public void keyReleased(KeyEvent e) { }
         });
     }
 
@@ -239,12 +256,31 @@ public class MainForm extends BaseForm {
         PrepareIntro();
         currentThread = new Thread(() -> {
             try {
-                Thread.sleep(12000);
+                Thread.sleep(6000);
             } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                StartSecondIntroThread();
+            }
+            StartSecondIntroThread();
+        });
+        currentThread.start();
+    }
+
+    private void StartSecondIntroThread(){
+        PrepareIntro();
+        currentThread = new Thread(() -> {
+            try {
+                creditsPanel.setVisible(true);
+                kyrLabel.setVisible(false);
+                ruLabel.setVisible(false);
+                engLabel.setVisible(false);
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                lessonStarted = true;
+                StartMotivatorsThread(0);
                 Thread.currentThread().interrupt();
             }
             lessonStarted = true;
-            System.out.println("Lesson started");
             StartMotivatorsThread(0);
         });
         currentThread.start();
@@ -253,21 +289,26 @@ public class MainForm extends BaseForm {
     private void StartMotivatorsThread(int startpos) {
         PrepareMotivators();
         final List<String> lines = new ContentLoader().GetFile("motivators.txt");
+        int length = lines.toArray().length;
         currentThread = new Thread(() -> {
             int counter = startpos;
             while (counter < 120 && !Thread.currentThread().isInterrupted()){
                 if(paused) continue;
-                int finalCounter = counter;
-                lines.forEach(s -> {
+
+                int item = 0;
+                while(item < length) {
+                    if(paused) continue;
                     if(Thread.currentThread().isInterrupted()) return;
-                    kyrLabel.setText(s);
+                    kyrLabel.setText(lines.get(item));
                     try {
-                        UpdateProgress(finalCounter / 120f, 0, 100);
+                        UpdateProgress(counter / 120f, 0, 100);
                         Thread.sleep(96);
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                     }
-                });
+                    item++;
+                }
+
                 counter++;
             }
             if(Thread.currentThread().isInterrupted()) return;
@@ -382,9 +423,7 @@ public class MainForm extends BaseForm {
         currentThread.start();
     }
 
-    private boolean IsPlaying() {
-        return currentMusicClip != null;
-    }
+    private boolean isPlaying;
     private Clip currentMusicClip = null;
     private long pausedClipTime = 0;
 
@@ -392,7 +431,8 @@ public class MainForm extends BaseForm {
         try {
             if(pausedClipTime > 0) {
                 currentMusicClip.start();
-            } else if (!IsPlaying()) {
+            } else if (!isPlaying) {
+                isPlaying = true;
                 currentMusicClip = AudioSystem.getClip();
                 URL path = classLoader.getResource("music.wav");
                 AudioInputStream ais = AudioSystem.getAudioInputStream(path);
@@ -407,22 +447,24 @@ public class MainForm extends BaseForm {
     }
 
     private void StopMusic(){
-        if(!IsPlaying()) return;
+        if(!isPlaying) return;
         currentMusicClip.close();
         currentMusicClip.stop();
         currentMusicClip.flush();
         pausedClipTime = 0;
         currentMusicClip = null;
+        isPlaying = false;
     }
 
     private void PauseMusic(){
-        if(!IsPlaying()) return;
+        if(!isPlaying) return;
         pausedClipTime = currentMusicClip.getMicrosecondPosition();
         currentMusicClip.stop();
     }
 
     private void UpdateProgress(float current, int stagemin, int stagemax) {
         int value = Mathf.Lerp(current, stagemin, stagemax);
+        System.out.println("updated progress to " + stagemin + value);
         progressSlider.setValue(stagemin + value);
     }
 
